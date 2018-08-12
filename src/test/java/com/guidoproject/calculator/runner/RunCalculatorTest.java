ff2d7b5cea62a5e8453ebd2c8121b4dea9f32d92
Guido <guido.quispe.4@outlook.com>
2018-08-12 15:40:51 -0400

    Plugin Json was add on .feature file.

--- src/test/java/com/guidoproject/calculator/runner/RunCalculatorTest.java
+++ src/test/java/com/guidoproject/calculator/runner/RunCalculatorTest.java
@@ -5,6 +5,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = { "pretty"}, glue = "com.guidoproject.calculator.steps", features = "classpath:cucumber/calculator.feature")
+@CucumberOptions(format = { "pretty"},
+                plugin = {"json:target/cucumber.json"},
+                glue = "com.guidoproject.calculator.steps", 
+                features = "classpath:cucumber/calculator.feature")
 public class RunCalculatorTest {
 }
